1574b2f354872e11eed7d3966c0218fdd2d26541
Unknown <feliperibeiro.ufu@gmail.com>
2019-02-18 22:18:32 -0300

    Mais andamentos no layout

--- app/src/main/java/com/TucuruiProductions/reanima_cardio/MainActivity.java
+++ app/src/main/java/com/TucuruiProductions/reanima_cardio/MainActivity.java
@@ -1,13 +1,24 @@
 package com.TucuruiProductions.reanima_cardio;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    String Array_name[] = {"ajuda na respiração"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//
+
     }
+
 }
